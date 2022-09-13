bc546674e9a3e109fa553b313c164f165a7486fa
MatheusOliveira1996 <matheus.hortolandia@gmail.com>
2022-09-13 20:02:45 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace concatenacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 32;
+            double saldo = 10.35784;
+            String nome = "Maria";
+
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+           
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+            
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a "
+            + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+        }
+    }
+}
